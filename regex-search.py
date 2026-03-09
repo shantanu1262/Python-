fdc786f6b95e93eab16885573a2c65d547ebb1f8
Shantanu Pal <157683351+shantanu1262@users.noreply.github.com>
2026-03-09 16:12:33 +0530

    Update regex-search.py

--- regex-search.py
+++ regex-search.py
@@ -1,8 +1,8 @@
 import re
-
+print("Hi, I am the smartBot, How can I help you !")
 user_input = input("you : ")
 if re.search ("Hi", user_input , re.IGNORECASE) :
     print("Bot : Hello User !")
-elif re.search("How are you " , user_input ,re.IGNORECASE) :
+elif re.search("How are you" , user_input ,re.IGNORECASE) :
     print("Bot : I am fine ! How you doing !")
-else : print("Sorry! Didn't get you , Please repeat again ")
+else : print("Sorry! Didn't get you , Please repeat again")
